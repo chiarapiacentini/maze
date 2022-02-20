a2af58e83af4421a3c82f0f02ed41cd740055e12
Chiara Piacentini <chiara@augmenta.ai>
2022-02-20 00:12:53 -0500

    clean up and add levels

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-console.log("Hello world!")
-
 class Position {
     constructor(x, y) {
         this.x = x;
@@ -13,14 +11,23 @@ const Direction = {
     UP: 2,
     LEFT: 3,
     INVALID: -1,
-  }
+}
 
 class Maze {
     constructor(width, height) {
+        // number of columns, from left to right
         this.width = width;
+
+        // number of rows, from top to bottom
         this.height = height;
+
+        // total number of cells
         this.n = this.width * this.height;
-        this.edges = new Array(this.n * 2).fill(true);
+
+        // edges are stored as a one dimensional vector, with 2 * n elements, where n is the number of cells.
+        // For every cell, we store if the down and the right direction is allowed.element 0, corresponds to the down 
+        // direction of cell(0, 0), element 1 down direction of cell(1, 0), etc.
+        this.edges = new Array(this.n * 2).fill(false);
     }
 
     get_cell_id(x, y) {
@@ -33,61 +40,31 @@ class Maze {
         return new Position(x, y);
     }
 
-    get_successor(cell_id, direction) {
-        const s = this.get_position(cell_id);
-        if (direction == Direction.DOWN && s.y < this.height - 1)
-            return this.get_cell_id(s.x, s.y + 1)
-        if (direction == Direction.RIGHT && s.x < this.width - 1)
-            return this.get_cell_id(s.x + 1, s.y)
-        if (direction == Direction.UP && s.y > 0)
-            return this.get_cell_id(s.x, s.y - 1)
-        if (direction == Direction.LEFT && s.x > 0)
-            return this.get_cell_id(s.x - 1, s.y)
+    get_neighbor(cell_id, direction) {
+        const position = this.get_position(cell_id);
+        if (this.exceed_bound(position, direction))
+            return cell_id;
+        if (direction == Direction.DOWN)
+            return this.get_cell_id(position.x, position.y + 1)
+        if (direction == Direction.RIGHT)
+            return this.get_cell_id(position.x + 1, position.y)
+        if (direction == Direction.UP)
+            return this.get_cell_id(position.x, position.y - 1)
+        if (direction == Direction.LEFT)
+            return this.get_cell_id(position.x - 1, position.y)
         return cell_id;
     }
 
-    get_successors(cell_id) {
-        let successors = new Array();
-        for (const direction  of Object.values(Direction) )
-        {
-            const successor = this.get_successor(cell_id, direction);
-            if (successor != cell_id) {
-                successors.push(successor);
-            }
-        }
-        return successors;
-    }
-
-    move(cell_id, direction) {
-        const edge_id = this.get_edge_id(cell_id, direction);
-        if (this.edges[edge_id])
-            return this.get_successor(cell_id, direction);
-        return cell_id;
-
-    }
-
-    get_moves(cell_id) {
-        let successors = new Array();
-        for (const direction  of Object.values(Direction) ){
-            const successor = this.move(cell_id, direction);
-            if (successor != cell_id) {
-                successors.push(successor);
-            }
-        }
-        return successors;
-    }
-
-
-    get_edge_id(cell_id, direction) {
-        const successor = this.get_successor(cell_id, direction);
-        const id = Math.min(cell_id, successor)
-        return id + this.n * (direction % 2);
-    }
-
-    set_obstacle(x, y, direction) {
-        const cell_id = this.get_cell_id(x, y);
-        const edge_id = this.get_edge_id(cell_id, direction);
-        this.edges[edge_id] = false;
+    exceed_bound(position, direction) {
+        if (direction == Direction.DOWN)
+            return position.y >= this.height - 1;
+        if (direction == Direction.RIGHT)
+            return position.x >= this.width - 1;
+        if (direction == Direction.UP)
+            return position.y <= 0;
+        if (direction == Direction.LEFT)
+            return position.x <= 0;
+        return true;
     }
 
     get_direction(pred, succ) {
@@ -104,6 +81,48 @@ class Maze {
         return Direction.INVALID;
     }
 
+    get_neighbors(cell_id) {
+        let successors = new Array();
+        for (const direction of Object.values(Direction)) {
+            const successor = this.get_neighbor(cell_id, direction);
+            if (successor != cell_id) {
+                successors.push(successor);
+            }
+        }
+        return successors;
+    }
+
+    get_edge_id(cell_id, direction) {
+        const successor = this.get_neighbor(cell_id, direction);
+        const id = Math.min(cell_id, successor)
+        return id + this.n * (direction % 2);
+    }
+
+    move(cell_id, direction) {
+        const edge_id = this.get_edge_id(cell_id, direction);
+        if (this.edges[edge_id])
+            return this.get_neighbor(cell_id, direction);
+        return cell_id;
+
+    }
+
+    get_moves(cell_id) {
+        let successors = new Array();
+        for (const direction of Object.values(Direction)) {
+            const successor = this.move(cell_id, direction);
+            if (successor != cell_id) {
+                successors.push(successor);
+            }
+        }
+        return successors;
+    }
+
+    set_obstacle(pred, succ) {
+        const dir = this.get_direction(pred, succ);
+        const edge_id = this.get_edge_id(pred, dir);
+        this.edges[edge_id] = false;
+    }
+
     set_free(pred, succ) {
         const dir = this.get_direction(pred, succ);
         const edge_id = this.get_edge_id(pred, dir);
@@ -115,8 +134,92 @@ class Maze {
         return this.edges[edge_id];
     }
 
-    printLog() {
-        console.log("Create a maze with " + String(this.width) + " x " + String(this.height) + " cells")
+    reachable_cells(x, y) {
+        const source = this.get_cell_id(x, y);
+        // dijkstra algorithm
+        const n = this.n
+        const inf = n * n + 1
+        let distance = new Array(n).fill(inf)
+        let predecessor = new Array(n).fill(-1)
+        let queue = new Array()
+        let visited = new Set()
+        visited.add(source)
+        distance[source] = 0
+        for (let i = 0; i < n; ++i) {
+            queue.push(i)
+        }
+
+        while (queue.length != 0) {
+
+            let min_ele = -1;
+            let min_priority = inf;
+            let min_index = -1;
+            for (let j = 0; j < queue.length; ++j) {
+                const ele = queue[j];
+                if (distance[ele] < min_priority) {
+                    min_priority = distance[ele];
+                    min_ele = ele;
+                    min_index = j;
+                }
+            }
+            const u = min_ele
+
+            // now remove
+            if (min_index > -1) {
+                queue.splice(min_index, 1);
+            }
+
+            const successors = this.get_moves(u);
+            if (successors.length == 0)
+                break;
+
+            for (let i = 0; i < successors.length; ++i) {
+                const v = successors[i];
+                if (visited.has(v))
+                    continue
+                const cost = distance[u] + 1;
+                if (cost < distance[v]) {
+                    distance[v] = cost;
+                    predecessor[v] = u;
+                    visited.add(v);
+                }
+            }
+        }
+        return distance.filter(x => x < inf);
+    }
+
+    pick_targets(initial_position, n_targets)
+    {
+        let targets = new Array(n_targets);
+        const explored = this.reachable_cells(initial_position.x, initial_position.y);
+        for (let i = 0; i < n_targets; ++i) {
+            const selected_cell = explored[Math.floor(Math.random() * explored.length)];
+            targets[i] = selected_cell;
+        }
+        return targets;
+    }
+}
+
+class Game {
+    constructor(width, height, initial_position, maze_builder, n_targets) {
+        this.maze = maze_builder(width, height);
+        this.score = 0;
+        this.n_targets = n_targets;
+        this.position = this.maze.get_cell_id(initial_position.x, initial_position.y);
+        // select random goals
+        this.targets = this.maze.pick_targets(initial_position, this.n_targets);
+    }
+
+    move(direction) {
+        this.position = this.maze.move(this.position, direction);
+        if (this.position == this.targets[this.score]) {
+            ++this.score;
+        }
+    }
+
+    win()
+    {
+        return this.score == this.n_targets && this.position == this.targets[this.n_targets - 1];
     }
 
 }
@@ -126,32 +229,30 @@ function generate_maze_random(x, y, threshold) {
     for (let i = 0; i < maze.n * 2; ++i) {
         let p = Math.random();
         if (p < threshold)
-            maze.edges[i] = false;
+            maze.edges[i] = true;
     }
     return maze;
 }
 
-function generate_maze_dpf(x, y) {
-    let maze = new Maze(x, y);
-    let visited = new Array(maze.n).fill(false);
-    maze.edges.fill(false);
+function generate_maze_dpf(width, height) {
+    // generate maze 
+    let maze = new Maze(width, height);
+
+    // start of exploration 
     const initial_cell = maze.get_cell_id(0, 0);
+
+    // set visited list and queue
+    let visited = new Array(maze.n).fill(false);
     let stack = new Array();
+
     visited[initial_cell] = true;
     stack.push(initial_cell);
-    console.log(stack.length);
-    // return maze;
-    const max_it = 2000;
-    let it = 0;
+
     while (stack.length != 0) {
-        console.log("Iteration " + String(it) + " " + String(stack.length));
-        if (it > max_it)
-            break;
-        ++it;
-        console.log("  stack length " + String(stack.length));
         let cell = stack.pop();
-        console.log("  stack length " + String(stack.length));
-        let successors = maze.get_successors(cell);
+        let successors = maze.get_neighbors(cell);
+
+        // check if the successors have been visited
         let unexplored = new Array();
         for (let i = 0; i < successors.length; ++i) {
             const successor = successors[i]
@@ -159,26 +260,26 @@ function generate_maze_dpf(x, y) {
                 unexplored.push(successor);
             }
         }
-        console.log(" length unexplored " + String(unexplored.length));
 
         if (unexplored.length != 0) {
+            // readd note to the stack
             stack.push(cell);
+
+            // select a random neighboring cell 
             const selected_cell = unexplored[Math.floor(Math.random() * unexplored.length)];
-            console.log(" selected_cell " + String(selected_cell));
+
+            // add edge
             maze.set_free(cell, selected_cell);
             visited[selected_cell] = true;
             stack.push(selected_cell);
         }
 
     }
-    for (let i = 0; i < maze.n * 2; ++i) {
-        console.log("Edge " + String(i) + " " + String(maze.edges[i]));
-    }
     return maze;
 }
 
-function visualize_maze(maze) {
-    const container = document.querySelector(".maze");
+function visualize_maze(maze, level) {
+    const container = document.querySelector("." + level + " .maze");
     for (let y = 0; y < maze.height; ++y) {
         const row = document.createElement("div");
         row.style.display = "flex";
@@ -210,156 +311,71 @@ function visualize_maze(maze) {
     }
 }
 
-function draw_position(maze, x, y, color = "green") {
-    const cell_id = maze.get_cell_id(x, y);
+function draw_position(cell_id, color = "green") {
     const cell = document.querySelector(".cell" + String(cell_id));
     cell.style.backgroundColor = color;
 }
 
-function clear_position(maze, x, y) {
-    const cell_id = maze.get_cell_id(x, y);
+function clear_position(cell_id) {
     const cell = document.querySelector(".cell" + String(cell_id));
     cell.style.backgroundColor = "white";
 }
-
-function get_color(maze, x, y)
-{
-    const cell_id = maze.get_cell_id(x, y);
+function get_color(cell_id) {
     const cell = document.querySelector(".cell" + String(cell_id));
     return cell.style.backgroundColor;
 }
 
-function explore_maze(maze, x, y) {
-    const source = maze.get_cell_id(x, y);
-    // dijkstra algorithm
-    const n = maze.n
-    const inf = n * n + 1
-    let distance = new Array(n).fill(inf)
-    let predecessor = new Array(n).fill(-1)
-    let queue = new Array()
-    let visited = new Set()
-    visited.add(source)
-    distance[source] = 0
-    for (let i = 0; i < n; ++i) {
-        queue.push(i)
-    }
+///
+let size = 5;
 
-    const max_it = 100;
-    let it = 0;
-    while (queue.length != 0) {
-
-        if (it > max_it)
-            break;
-        ++it;
-        let min_ele = -1;
-        let min_priority = inf;
-        let min_index = -1;
-        for (let j = 0; j < queue.length; ++j) {
-            const ele = queue[j];
-            if (distance[ele] < min_priority) {
-                min_priority = distance[ele];
-                min_ele = ele;
-                min_index = j;
-            }
-        }
-        const u = min_ele
-
-        // now remove
-        if (min_index > -1) {
-            queue.splice(min_index, 1);
-        }
-
-        const successors = maze.get_moves(u);
-        if (successors.length == 0)
-            break;
-        
-        for (let i = 0; i < successors.length; ++i) {
-            const v = successors[i];
-            if (visited.has(v))
-                continue
-            const cost = distance[u] + 1;
-            if (cost < distance[v]) {
-                distance[v] = cost;
-                predecessor[v] = u;
-                visited.add(v);
-            }
-        }
-    }
-    return distance.filter( x => x < inf);
-}
-
-
-let reached_goals = 0;
-const n_goals = 3;
-let goals = new Array(n_goals);
-
-function generate_game()
-{
-    reached_goals = 0;
-    let maze = generate_maze_dpf(6, 6);
-    const explored = explore_maze(maze, 0, 0);
-
-    // pick three goals
-    goals = new Array(n_goals);
-    for (let i = 0; i < n_goals; ++i) {
-        const selected_cell = explored[Math.floor(Math.random() * explored.length)];
-        goals[i] = selected_cell;
-    }    
-    return maze;
-}
-
-maze = generate_game()
-let position = new Position(0, 0);
-visualize_maze(maze);
 let colors = new Array("blue", "purple", "red");
-
-draw_position(maze, position.x, position.y);
-for (let i = 0; i < n_goals; ++i) {
-    const goal = goals[i]
-    const p = maze.get_position(goal);
-    draw_position(maze, p.x, p.y, colors[i]);
-}    
+let game = new Game(size, size, new Position(0, 0), generate_maze_dpf, 3);
+visualize_maze(game.maze, "level1");
+visualize_maze(game.maze, "level2");
+visualize_maze(game.maze, "level3");
+visualize_maze(game.maze, "level4");
+draw_position(game.position);
+for (let i = 0; i < game.n_targets; ++i) {
+    console.log("target " + String(game.targets[i]));
+    draw_position(game.targets[i], colors[i]);
+}
 
 let previous_color = "white";
 
 function update_position(move) {
-    position_id = maze.get_cell_id(position.x, position.y);
-    console.log("Move " + String(position.x) + " " + String(position.y) + " " + String(move),)
-    successor_id = maze.move(position_id, move);
-    draw_position(maze, position.x, position.y, previous_color);
-    position = maze.get_position(successor_id);
-    console.log("  to " + String(position.x) + " " + String(position.y))
-    previous_color = get_color(maze, position.x, position.y);
-    draw_position(maze, position.x, position.y, "green");
-    if (successor_id == goals[reached_goals]) {
-        ++reached_goals;
+    const position = game.position;
+    const score = game.score;
+    draw_position(position, previous_color);
+    game.move(move);
+    previous_color = get_color(game.position);
+    draw_position(game.position, "green");
+    if (score != game.score) {
         previous_color = "white"
     }
-    if (reached_goals == n_goals)
-    {
+    if (game.win()) {
         alert("You win!");
-        }
+    }
 }
 
 
 const clearMaze = document.getElementById('clear');
 clearMaze.addEventListener('click', () => {
-    
-    clear_position(maze, position.x, position.y);
+
     const div = document.querySelector(".maze");
+    draw_position(game.position, "white");
+    for (let i = 0; i < game.n_targets; ++i) {
+        draw_position(game.targets[i], "white");
+    }
     while (div.firstChild) {
         div.removeChild(div.firstChild);
     }
-    maze = generate_game()
-    position = new Position(0, 0);
-    visualize_maze(maze);
-    for (let i = 0; i < n_goals; ++i) {
-        const goal = goals[i]
-        const p = maze.get_position(goal);
-        draw_position(maze, p.x, p.y, colors[i]);
+    size++;
+    game = new Game(size, size, new Position(0, 0), generate_maze_dpf, 3)
+    visualize_maze(game.maze);
+    draw_position(game.position);
+    for (let i = 0; i < game.n_targets; ++i) {
+        draw_position(game.targets[i], colors[i]);
     }
-    position = new Position(0, 0);
-    draw_position(maze, position.x, position.y);
 });
 
 const moveLeft = document.getElementById('left');
